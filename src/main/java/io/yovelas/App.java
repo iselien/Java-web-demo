93ae3cc916051e544c100493bbee789033aa5b65
yovelas <yovelas@163.com>
2020-10-27 17:56:12 +0800

    add jpa

--- src/main/java/io/yovelas/App.java
+++ src/main/java/io/yovelas/App.java
@@ -1,7 +1,13 @@
 package io.yovelas;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  * Spring Boot Starter
@@ -9,10 +15,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableJpaRepositories("io.yovelas")
 public class App {
+
+
+    @Autowired
+    PagingAndSortingRepository pagingAndSortingRepository;
+
+    public App(PagingAndSortingRepository pagingAndSortingRepository) {
+        this.pagingAndSortingRepository = pagingAndSortingRepository;
+    }
+
+    @RequestMapping("/")
+    public void index(){
+        PagingAndSortingRepository<User, Long> repository =  pagingAndSortingRepository;
+        Page<User> users = repository.findAll(PageRequest.of(1, 20));
+        System.out.println(users);
+        System.out.println("ss");
+        return;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+
 
 }
